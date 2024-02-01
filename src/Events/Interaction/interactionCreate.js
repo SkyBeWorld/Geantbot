dcad597d0184578c73a5c89319465790425ad376
GeantW0rld <106020834+GeantW0rld@users.noreply.github.com>
2024-02-01 09:07:17 +0100

    create interactionCreate.js

---
+++ src/Events/Interaction/interactionCreate.js
@@ -0,0 +1,12 @@
+const { Client, ActivityType, Status, Events } = require("discord.js")
+
+module.exports = {
+    name: Events.InteractionCreate,
+    once: true,
+    /**
+     * @param {Client} client
+     */
+    async execute(client) {
+        
+    }
+}
